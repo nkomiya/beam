d6181fc9b7795e53ffdd972ef185aa4b33441ac4
nkomiya <n.comiya@gmail.com>
2019-11-10 20:00:12 +0900

    build.sh のバグ修正
    /doc に出力先フォルダが作成されていない場合への対応

--- build.sh
+++ build.sh
@@ -16,45 +16,45 @@ build() {
     [ $# -eq 2 ] && opt="-maxdepth $2"
     #
     if [ ! -z "$1" ]; then
-	[ ! -d "$1" ] && mkdir -p "$1"
-	cd $1
+    	[ ! -d "$1" ] && mkdir -p "$1"
+    	cd $1
     fi
 
     pathes=()
     for ifile in $(find ${mdPath} -type f -name "*.md" ${opt}); do
 	ofile=$(realpath --relative-to=${mdPath} ${ifile} | sed "s/\.md/.html/")
 	dname=$(dirname $ofile)
-	cssRelat=$(realpath --relative-to=${dname} ${cssfile})
 
 	[ "${dname}" != "." ] && [ ! -d "${dname}" ] && mkdir -p ${dname}
+	cssRelat=$(realpath --relative-to=${dname} ${cssfile})
 
-	echo "<script src=\"$(realpath --relative-to=${dname} ${jsfile})\"></script>" > tmp
-	sed -e "s/\.md/.html/" ${ifile}  | \
-	    pandoc -s -t html5 -c ${cssRelat} -o ${ofile} -H tmp \
-		   --metadata pagetitle="Ja: Beam Programming Guide"
-	rm tmp
-	sed -i "" \
-	    -e "s/pre class=\"sourceCode java\"/pre class=\"line-numbers\"/g" \
-	    -e "s/code class=\"sourceCode java\"/code class=\"language-java\"/g" \
-	    -e "s/pre class=\"sourceCode bash\"/pre class=\"line-numbers\"/g" \
-	    -e "s/code class=\"sourceCode bash\"/code class=\"language-bash\"/g" \
-	    -e "s/pre class=\"sourceCode xml\"/pre class=\"line-numbers\"/g" \
-	    -e "s/code class=\"sourceCode xml\"/code class=\"language-xml\"/g" \
-	    -e "s/pre class=\"sourceCode json\"/pre class=\"line-numbers\"/g" \
-	    -e "s/code class=\"sourceCode json\"/code class=\"language-json\"/g" \
-	    ${ofile}
-	#
-	pathes+=("${dname}")
+    	echo "<script src=\"$(realpath --relative-to=${dname} ${jsfile})\"></script>" > tmp
+    	sed -e "s/\.md/.html/" ${ifile}  | \
+    	    pandoc -s -t html5 -c ${cssRelat} -o ${ofile} -H tmp \
+    		   --metadata pagetitle="Ja: Beam Programming Guide"
+    	rm tmp
+    	sed -i "" \
+    	    -e "s/pre class=\"sourceCode java\"/pre class=\"line-numbers\"/g" \
+    	    -e "s/code class=\"sourceCode java\"/code class=\"language-java\"/g" \
+    	    -e "s/pre class=\"sourceCode bash\"/pre class=\"line-numbers\"/g" \
+    	    -e "s/code class=\"sourceCode bash\"/code class=\"language-bash\"/g" \
+    	    -e "s/pre class=\"sourceCode xml\"/pre class=\"line-numbers\"/g" \
+    	    -e "s/code class=\"sourceCode xml\"/code class=\"language-xml\"/g" \
+    	    -e "s/pre class=\"sourceCode json\"/pre class=\"line-numbers\"/g" \
+    	    -e "s/code class=\"sourceCode json\"/code class=\"language-json\"/g" \
+    	    ${ofile}
+    	#
+    	pathes+=("${dname}")
     done
 
     # images
     local from to
     for dname in ${pathes[@]}; do
-	from=$(cd ${mdPath} && cd ${dname} && pwd)/figs
-	if [ -d  "${from}" ]; then
-	    [ ! -d ${dname}/figs ] && mkdir -p ${dname}/figs
-	    cp ${from}/* ${dname}/figs/
-	fi
+    	from=$(cd ${mdPath} && cd ${dname} && pwd)/figs
+    	if [ -d  "${from}" ]; then
+    	    [ ! -d ${dname}/figs ] && mkdir -p ${dname}/figs
+    	    cp ${from}/* ${dname}/figs/
+    	fi
     done
 
     cd ${docdir}
